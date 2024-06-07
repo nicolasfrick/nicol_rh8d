7f0d32503c5d00f67ede834ea5cee3b8a7e2906e
Nicolas Frick <nicolas.frick@uni-hamburg.de>
2024-06-07 16:38:54 +0200

    test hand detection

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
 # Installation
 
+## Install miniconda3
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+
+## Install env
 Follows nicol_demos/commands_env.txt NICOL Frozen Environment
 
 ENV=nicol_rh8d
-PYTHON=3.10
-DATASETS_DIR=/home/nicol/repositories/nicol_rh8d/datasets/OpenMM/data
-NICOL_RH8D=/home/nicol/repositories/nicol_rh8d
+PYTHON=3.9
+DATASETS_DIR=~/nicol_rh8d/datasets/OpenMM/data
+NICOL_RH8D=~/nicol_rh8d
 conda create -n $ENV python=$PYTHON
 conda activate $ENV
-conda install -c pytorch -c nvidia numpy=1.22 pytorch-cuda=11.7 pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 'libtiff<4.5'
-conda install future pybind11
-# OpenMM
+
+## PyTorch
+conda install -c pytorch numpy=1.22 cudatoolkit=10.2 pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 torchtext==0.13.1 'libtiff<4.5'
+
+## OpenMM
 pip install --upgrade openmim
 mim install 'mmtrack<1' 'mmpose<1' 'mmdet<3' mmcls 'mmcv-full<1.7'
+mim list
+pip uninstall xtcocotools  # <-- Otherwise import mmpose.apis fails...
+pip install --no-binary xtcocotools xtcocotools
+pip check
+
+## Config
+ln -s "$DATASETS_DIR" "$CONDA_PREFIX"/lib/python$PYTHON/site-packages/mmdet/.mim/data
+ln -s "$DATASETS_DIR" "$CONDA_PREFIX"/lib/python$PYTHON/site-packages/mmpose/.mim/data
+ln -s "$CONDA_PREFIX"/lib/python$PYTHON/site-packages/tests "$CONDA_PREFIX"/lib/python$PYTHON/site-packages/mmpose/.mim/tests
+
+
+
--- scripts/keypoint_detect.py
+++ scripts/keypoint_detect.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python3
 
-
-import cv2
+import os, cv2
 import ppyutil.image_plot
 from pose_detector import PoseDetector
 import warnings
 warnings.filterwarnings('ignore')
 
-MMDETDIR = '/home/allgeuer/Programs/DeepStack/envs/mmdev/mmdetection/'
-MMPOSEDIR = '/home/allgeuer/Programs/DeepStack/envs/mmdev/mmpose/'
+HOME = os.path.expanduser("~")
+CONDA = os.environ['CONDA_PREFIX']
+MMDETDIR = os.path.join(CONDA,'lib/python3.9/site-packages/mmdet/.mim')
+MMPOSEDIR = os.path.join(CONDA,'lib/python3.9/site-packages/mmpose/.mim')
 
 def plot_images(*images):
     ppyutil.image_plot.plot_images(tuple(image[:,:,::-1] for image in images))
+
+with PoseDetector(
+        det=(os.path.join(MMPOSEDIR, 'demo/mmdetection_cfg/cascade_rcnn_x101_64x4d_fpn_1class.py'), 'https://download.openmmlab.com/mmpose/mmdet_pretrained/cascade_rcnn_x101_64x4d_fpn_20e_onehand10k-dac19597_20201030.pth'),
+        # dd=(os.path.join(MMPOSEDIR, 'configs/hand/2d_kpt_sview_rgb_img/topdown_heatmap/onehand10k/res50_onehand10k_256x256.py'), 'https://download.openmmlab.com/mmpose/top_down/resnet/res50_onehand10k_256x256-e67998f6_20200813.pth'),
+        dd=(os.path.join(MMPOSEDIR, 'configs/hand/2d_kpt_sview_rgb_img/topdown_heatmap/onehand10k/hrnetv2_w18_onehand10k_256x256_dark.py'), 'https://download.openmmlab.com/mmpose/top_down/resnet/res50_onehand10k_256x256-e67998f6_20200813.pth'),
+    ) as detector:
+    
+    while input("Press key to scan, q to escape") != 'q':
+        demo_hands = cv2.imread(os.path.join(HOME, 'nicol_rh8d/datasets/OpenMM/data/5.jpeg'))
+        # demo_hands = cv2.resize(demo_hands, (256,256))
+        result, _ = detector.process_image(demo_hands, output_vis=True)
+        plot_images(result.vis_image.det, result.vis_image.dd)
