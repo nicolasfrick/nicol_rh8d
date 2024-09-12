e25cdcad2edef5b69d0917ec2a03418873cd6f44
Nicolas Frick <9frick@informatik.uni-hamburg.de>
2024-09-12 18:28:02 +0200

    test filter

--- scripts/pose_filter.py
+++ scripts/pose_filter.py
@@ -2,6 +2,8 @@
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from typing import Tuple, Optional
 from scipy.spatial.transform import Rotation as R
 
 class PoseFilter():
@@ -12,29 +14,46 @@ class PoseFilter():
 
 		@param f_ctrl Loop frequency
 		@type float
+		@param state_init Initialize the states of the filter (x,y,z,r,p,y)
+		@type Tuple[float,float,float,float,float,float]
 		@param num_states Number of filter states (visible + hidden)
 		@type int
 		@param num_measurements Number of input parameters
 		@type int
+		@param process_noise Noise model initialization for process
+		@type float
+		@param measurement_noise Noise model initialization for measurement
+		@type float
 	"""
 
 	def __init__(self,
 			  				f_ctrl: float,
-							num_states: int=18,
-							num_measurements: int=6,
-							process_noise: float=1e-5,
-							measurement_noise: float=1e-2) -> None:
+							state_init: Optional[Tuple[float,float,float,float,float,float]]=(0.0,0.0,0.0,0.0,0.0,0.0),
+							num_states: Optional[int]=18,
+							num_measurements: Optional[int]=6,
+							process_noise: Optional[float]=1e-5,
+							measurement_noise: Optional[float]=1e-2) -> None:
 		
 		self.dt = 1/f_ctrl
+		self.state_init = state_init
 		self.num_states = num_states
 		self.num_measurements = num_measurements
 		self.process_noise = process_noise
 		self.measurement_noise = measurement_noise
-
+		self.translation_estimated = np.zeros(3, dtype=np.float32)
+		self.rotation_estimated = np.zeros(3, dtype=np.float32)
 		self.filter = cv2.KalmanFilter(self.num_states, self.num_measurements)
-		self.measurements = np.zeros(self.num_measurements, dtype=np.float32)
-
 		self.initFilter()
+
+	@property
+	def rotation_as_euler(self) -> np.ndarray:
+		return self.rotation_estimated
+	@property
+	def rotation_as_matrix(self) -> np.ndarray:
+		return  R.from_euler('xyz', self.rotation_estimated) 
+	@property
+	def translation(self) -> np.ndarray:
+		return self.translation_estimated
 
 	def initFilter(self) -> None:
 		"""        
@@ -75,89 +94,97 @@ class PoseFilter():
 		self.filter.measurementNoiseCov = cv2.setIdentity(self.filter.measurementNoiseCov, self.measurement_noise)
 
 		# transition matrix
+		transition_matrix = np.eye(self.num_states, dtype=np.float32)
 		# position
-		self.filter.transitionMatrix[0, 3] = self.dt
-		self.filter.transitionMatrix[1, 4] = self.dt
-		self.filter.transitionMatrix[2, 5] = self.dt
-		self.filter.transitionMatrix[3, 6] = self.dt
-		self.filter.transitionMatrix[4, 7] = self.dt
-		self.filter.transitionMatrix[5, 8] = self.dt
-		self.filter.transitionMatrix[0, 6] = 0.5*pow(self.dt, 2)
-		self.filter.transitionMatrix[1, 7] = 0.5*pow(self.dt, 2)
-		self.filter.transitionMatrix[2, 8] = 0.5*pow(self.dt, 2)
+		transition_matrix[0, 3] = self.dt
+		transition_matrix[1, 4] = self.dt
+		transition_matrix[2, 5] = self.dt
+		transition_matrix[3, 6] = self.dt
+		transition_matrix[4, 7] = self.dt
+		transition_matrix[5, 8] = self.dt
+		transition_matrix[0, 6] = 0.5*pow(self.dt, 2)
+		transition_matrix[1, 7] = 0.5*pow(self.dt, 2)
+		transition_matrix[2, 8] = 0.5*pow(self.dt, 2)
 		# orientation
-		self.filter.transitionMatrix[9, 12] = self.dt
-		self.filter.transitionMatrix[10, 13] = self.dt
-		self.filter.transitionMatrix[11, 14] = self.dt
-		self.filter.transitionMatrix[12, 15] = self.dt
-		self.filter.transitionMatrix[13, 16] = self.dt
-		self.filter.transitionMatrix[14, 17] = self.dt
-		self.filter.transitionMatrix[9, 15] = 0.5*pow(self.dt, 2)
-		self.filter.transitionMatrix[10, 16] = 0.5*pow(self.dt, 2)
-		self.filter.transitionMatrix[11, 17] = 0.5*pow(self.dt, 2)
+		transition_matrix[9, 12] = self.dt
+		transition_matrix[10, 13] = self.dt
+		transition_matrix[11, 14] = self.dt
+		transition_matrix[12, 15] = self.dt
+		transition_matrix[13, 16] = self.dt
+		transition_matrix[14, 17] = self.dt
+		transition_matrix[9, 15] = 0.5*pow(self.dt, 2)
+		transition_matrix[10, 16] = 0.5*pow(self.dt, 2)
+		transition_matrix[11, 17] = 0.5*pow(self.dt, 2)
+		self.filter.transitionMatrix = transition_matrix
 
 		# measurement matrix
-		self.filter.measurementMatrix[0, 0] = 1  # x
-		self.filter.measurementMatrix[1, 1] = 1  # y
-		self.filter.measurementMatrix[2, 2] = 1  # z
-		self.filter.measurementMatrix[3, 9] = 1  # roll
-		self.filter.measurementMatrix[4, 10] = 1 # pitch
-		self.filter.measurementMatrix[5, 11] = 1 # yaw
+		measurement_matrix = np.zeros((self.num_measurements, self.num_states), dtype=np.float32)
+		measurement_matrix[0, 0] = 1  # x
+		measurement_matrix[1, 1] = 1  # y
+		measurement_matrix[2, 2] = 1  # z
+		measurement_matrix[3, 9] = 1  # roll
+		measurement_matrix[4, 10] = 1 # pitch
+		measurement_matrix[5, 11] = 1 # yaw
+		self.filter.measurementMatrix = measurement_matrix
 
-	def updateFilter(self, measurement):
+		# states
+		init_state = np.zeros((self.num_states, 1), dtype=np.float32)
+		init_state[0][0] = self.state_init[0]
+		init_state[1][0] = self.state_init[1]
+		init_state[2][0] = self.state_init[2]
+		init_state[9][0] = self.state_init[3]
+		init_state[10][0] = self.state_init[4]
+		init_state[11][0] = self.state_init[5]
+		self.filter.statePre = init_state
+		self.filter.statePost = init_state
+
+	def updateFilter(self, measurement: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
 		# predict, to update the internal statePre variable
 		prediction = self.filter.predict()
+		prediction = prediction.flatten()
 		# correct phase uses predicted value and measurement
 		estimated = self.filter.correct(measurement)
+		estimated = estimated.flatten()
 
-		# translation estimate
-		translation_estimated = np.zeros(3, dtype=np.float32)
-		translation_estimated[0] = estimated[0]
-		translation_estimated[1] = estimated[1]
-		translation_estimated[2] = estimated[2]
-		# euler angle estimate
-		euler_estimated = np.zeros(3, dtype=np.float32)
-		euler_estimated[0] = estimated[9]
-		euler_estimated[1] = estimated[10]
-		euler_estimated[2] = estimated[11]
-		# convert to rotation matrix
-		rotation_estimated = R.from_euler('xyz', euler_estimated)
+		self.translation_estimated = estimated[:3]
+		self.rotation_estimated = estimated[9:12]
+		return self.rotation_estimated, self.translation_estimated
 
-		return translation_estimated, rotation_estimated
-
-	def addMeasurement(self, rvec, tvec): #Mat &measurements,const Mat &translation_measured, const Mat &rotation_measured)
+	def toMeasurement(self, rvec: np.ndarray, tvec: np.ndarray) -> np.ndarray:
 		# convert rotation vector to euler angles
 		roudriges, _ = cv2.Rodrigues(np.array(rvec))
 		mat = R.from_matrix(roudriges)
 		measured_eulers = mat.as_euler('xyz')
 
-		# set measurement to predict
-		self.measurements[0] = tvec[0] # x
-		self.measurements[1] = tvec[1] # y
-		self.measurements[2] = tvec[2] # z
-		self.measurements[3] = measured_eulers[0] # roll
-		self.measurements[4] = measured_eulers[1] # pitch
-		self.measurements[5] = measured_eulers[2] # yaw
-
-	def  measure(self, inliers_idx):
-		if inliers_idx.rows >= self.minInliersKalman:
-				# Get the measured translation
-				# Mat translation_measured = pnp_detection.get_t_matrix();
-
-				# // Get the measured rotation
-				# Mat rotation_measured = pnp_detection.get_R_matrix();
-
-				# // fill the measurements vector
-				# self.fillMeasurements(measurements, translation_measured, rotation_measured)
-				self.good_measurement = True
-
-		# update the Kalman filter with good measurements, otherwise with previous valid measurements
-		# Mat translation_estimated(3, 1, CV_64FC1);
-		# Mat rotation_estimated(3, 3, CV_64FC1);
-		# updateKalmanFilter( KF, measurements,
-		#                     translation_estimated, rotation_estimated);
-		# -- Step 6: Set estimated projection matrix
-		# pnp_detection_est.set_P_matrix(rotation_estimated, translation_estimated);
+		measurement = np.zeros(self.num_measurements, dtype=np.float32)
+		measurement[:3] = tvec[:] 
+		measurement[3:] = measured_eulers[:]
+		return measurement
 
 if __name__=="__main__":
-	 f = PoseFilter()
+	fq, size = 30, 100
+	init = (0,0,0,0.1,0,0)
+	proc_noise = 0.1
+	meas_noise = 0.8
+	print(meas_noise)
+	f = PoseFilter(fq, init, process_noise=proc_noise, measurement_noise=meas_noise)
+
+	var = 3
+	error_2 = 0.01
+	est, inp = np.zeros((size, 6), dtype=np.float32), np.zeros((size, 6), dtype=np.float32)
+	for i in range(size):
+		meas = np.random.uniform(init[var]-error_2, init[var]+error_2, 6).astype(np.float32)
+		(rot, trans) = f.updateFilter(f.toMeasurement(meas[:3], meas[3:]))
+		inp[i] = meas
+		est[i,:3] = rot
+		est[i,3:] = trans
+
+	xs = [x/fq for x in range(size)]
+	mean = sum(inp[:, var])/len(inp[:, var])
+	print("Last estimation: ", est[-1,var], ", mean: ", mean, ", true value: ", init[var])
+	plt.plot(xs, inp[:, var], label='measurement')
+	plt.plot(xs, [init[var] for _ in range(size)], label='init')
+	plt.plot(xs, [mean for _ in range(size)], label='mean')
+	plt.plot(xs, est[:, var], label='estimate', linewidth=2)
+	plt.legend()
+	plt.show()
